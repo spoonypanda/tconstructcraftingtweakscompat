64fb88a37d19e22df336bb59c971d8bd25f32f7c
spoonypanda <42197654+spoonypanda@users.noreply.github.com>
2025-08-13 08:56:37 -0700

    Fixed an issue with api not registering properly server side for multiplayer

--- src/main/java/com/spoonypanda/tconstructcraftingtweakscompat/TConstructCraftingTweaksCompat.java
+++ src/main/java/com/spoonypanda/tconstructcraftingtweakscompat/TConstructCraftingTweaksCompat.java
@@ -1,11 +1,12 @@
 package com.spoonypanda.tconstructcraftingtweakscompat;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(TConstructCraftingTweaksCompat.MODID)
 public class TConstructCraftingTweaksCompat {
@@ -13,38 +14,34 @@ public class TConstructCraftingTweaksCompat {
     public static final int BUTTON_ADJUST_X = 25;
 
     public TConstructCraftingTweaksCompat() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modBus.addListener(this::enqueueIMC);
     }
 
-    private void onClientSetup(final FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            if (!ModList.get().isLoaded("craftingtweaks") || !ModList.get().isLoaded("tconstruct")) {
-                System.out.println("[TConstructCraftingTweaksCompat] Required mods not present, skipping integration.");
-                return;
-            }
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+        if (!ModList.get().isLoaded("craftingtweaks") || !ModList.get().isLoaded("tconstruct")) {
+            return;
+        }
 
-            try {
-                CompoundNBT tag = new CompoundNBT();
-                tag.putString("ContainerClass", "slimeknights.tconstruct.tables.inventory.table.CraftingStationContainer");
-                tag.putInt("GridSlotNumber", 1);
-                tag.putInt("GridSize", 9);
+        InterModComms.sendTo("craftingtweaks", "RegisterProvider", () -> {
+            CompoundNBT tag = new CompoundNBT();
+            tag.putString("ContainerClass", "slimeknights.tconstruct.tables.inventory.table.CraftingStationContainer");
+            tag.putInt("GridSlotNumber", 1);
+            tag.putInt("GridSize", 9);
 
-                CompoundNBT clear = new CompoundNBT();
-                clear.putInt("ButtonX", BUTTON_ADJUST_X);
-                tag.put("TweakClear", clear);
+            CompoundNBT clear = new CompoundNBT();
+            clear.putInt("ButtonX", BUTTON_ADJUST_X);
+            tag.put("TweakClear", clear);
 
-                CompoundNBT rotate = new CompoundNBT();
-                rotate.putInt("ButtonX", BUTTON_ADJUST_X);
-                tag.put("TweakRotate", rotate);
+            CompoundNBT rotate = new CompoundNBT();
+            rotate.putInt("ButtonX", BUTTON_ADJUST_X);
+            tag.put("TweakRotate", rotate);
 
-                CompoundNBT balance = new CompoundNBT();
-                balance.putInt("ButtonX", BUTTON_ADJUST_X);
-                tag.put("TweakBalance", balance);
+            CompoundNBT balance = new CompoundNBT();
+            balance.putInt("ButtonX", BUTTON_ADJUST_X);
+            tag.put("TweakBalance", balance);
 
-                InterModComms.sendTo("craftingtweaks", "RegisterProvider", () -> tag);
-            } catch (Throwable t) {
-                System.err.println("[TConstructCraftingTweaksCompat] Failed to register provider: " + t.getMessage());
-            }
+            return tag;
         });
     }
 }
